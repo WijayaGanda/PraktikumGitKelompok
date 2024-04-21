60ff7d0c7c3dc1635a83320119c7bdb5a63d3463
Pavita Pramestri <pavita.vita04@gmail.com>
2024-04-21 18:49:36 +0700

    edit update

--- resources/views/employee/edit.blade.php
+++ resources/views/employee/edit.blade.php
@@ -1,6 +1,5 @@
 @extends('layouts.app')
 @section('content')
-
     <div class="container-sm mt-5">
         <form action="{{ route('employees.update', ['employee' => $employee->id]) }}" method="POST">
             @csrf
@@ -8,7 +7,7 @@
             <div class="row justify-content-center">
                 <div class="p-5 bg-light rounded-3 border col-xl-6">
                     <div class="mb-3 text-center">
-                        <i class="bi-person-circle fs-1"></i>
+                        <i class="bi bi-person-fill-gear h1"></i>
                         <h4>Edit Employee</h4>
                     </div>
                     <hr>
@@ -33,8 +32,8 @@
                         </div>
                         <div class="col-md-6 mb-3">
                             <label for="email" class="form-label">Email</label>
-                            <input class="form-control @error('email') is-invalid @enderror" type="text"
-                                name="email" id="email" value="{{ $employee->email }}" placeholder="Enter Email">
+                            <input class="form-control @error('email') is-invalid @enderror" type="text" name="email"
+                                id="email" value="{{ $employee->email }}" placeholder="Enter Email">
                             @error('email')
                                 <span class="invalid-feedback">{{ $message }}</span>
                             @enderror
@@ -59,8 +58,7 @@
                                     }
                                 @endphp
                                 @foreach ($positions as $position)
-                                    <option value="{{ $position->id }}"
-                                        {{ $selected == $position->id ? 'selected' : '' }}>
+                                    <option value="{{ $position->id }}" {{ $selected == $position->id ? 'selected' : '' }}>
                                         {{ $position->code . '-' . $position->name }}</option>
                                 @endforeach
                             </select>
@@ -74,10 +72,11 @@
                     <div class="row">
                         <div class="col-md-6 d-grid">
                             <a href="{{ route('employees.index') }}" class="btn btn-outline-dark btn-lg mt-3"><i
-                                    class="bi-arrow-left-circle me-2"></i> Cancel</a>
+                                    class="bi bi-x-circle"></i> Cancel</a>
                         </div>
                         <div class="col-md-6 d-grid">
-                            <button type="submit" class="btn btn-dark btn-lg mt-3"><i class="bi-check-circle me-2"></i>
+                            <button type="submit" class="btn btn-outline-dark btn-lg mt-3"><i
+                                    class="bi bi-check-circle-fill"></i>
                                 Save</button>
                         </div>
                     </div>
